04b59521436a99fcb2e4adc3e6c964102e0c7358
M7Marcketplace <m7.contact.us@gmail.com>
2026-03-06 14:59:12 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoyCWY2dqtToqL-D2J25-fQ_xEJsuVX4s",
+  authDomain: "m7-marketplace.firebaseapp.com",
+  projectId: "m7-marketplace",
+  storageBucket: "m7-marketplace.firebasestorage.app",
+  messagingSenderId: "79271667417",
+  appId: "1:79271667417:web:5ede45e27931cbc2df80c8"
+};
+
+export const app = initializeApp(firebaseConfig);
