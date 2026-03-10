d2a1ab7d8ed34cba12446a674f2f911dd05e887a
M7Marcketplace <m7.contact.us@gmail.com>
2026-03-10 05:11:23 +0100

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'config.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="refresh" content="0; url=home.php">
+</head>
+<body>
+    <p>Redirecting to <a href="home.php">M7 Marketplace</a>...</p>
+</body>
+</html>
+
+
+
